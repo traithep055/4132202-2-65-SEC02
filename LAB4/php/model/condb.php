2a89dcdce3c2216a042370b49a6894fe8fcd9cd7
traithep055 <ais4255bk@gmail.com>
2022-12-08 12:10:37 +0700

    LAB4

---
+++ LAB4/php/model/condb.php
@@ -0,0 +1,12 @@
+<?php
+$host = "db"; //db service from docker-compose
+$user = "root";
+$pass = "1234";
+$db = "db_student";
+
+try {
+    $link = mysqli_connect($host, $user, $pass, $db);
+    mysqli_query($link, "SET NAMES utf8");
+} catch (Exception $e) {
+    echo "An error is" . $e . " :: " . mysqli_error($link);
+}
